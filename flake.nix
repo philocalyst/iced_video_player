6f6273772f93a2d337470c9bf34a310bd5140a1a
Myles Wirth <regime_skylark.0y@icloud.com>
2026-03-16 23:47:22 -0400

    Cleanup

--- flake.nix
+++ flake.nix
@@ -1,28 +1,37 @@
 {
+  description = "iced_video_player dev shell";
+
   inputs = {
-    flakeCompat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixCargoIntegration = {
-      url = "github:yusdacra/nix-cargo-integration";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    inputs.nixCargoIntegration.lib.makeOutputs {
-      root = ./.;
-      overrides = {
-        shell = common: prev: {
-          env = prev.env ++ [
-            {
-              name = "GST_PLUGIN_PATH";
-              value = "${common.pkgs.gst_all_1.gstreamer}:${common.pkgs.gst_all_1.gst-plugins-bad}:${common.pkgs.gst_all_1.gst-plugins-ugly}:${common.pkgs.gst_all_1.gst-plugins-good}:${common.pkgs.gst_all_1.gst-plugins-base}";
-            }
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            rustPlatform.bindgenHook
+          ];
+
+          buildInputs = with pkgs; [
+            glib
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
           ];
         };
-      };
-    };
+      }
+    );
 }
